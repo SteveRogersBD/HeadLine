e65444cfc2e189672409b5e6d957c88fdc3e9c02
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-03-19 15:02:18 -0500

    Changed parent theme

--- app/src/main/java/com/example/headline/SplashActivity.java
+++ app/src/main/java/com/example/headline/SplashActivity.java
@@ -5,21 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.headline.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Glide.with(this).asGif().load(R.drawable.headline).into(binding.headlineImgView);
+
         Thread td = new Thread(){
             public void run(){
                 try{
-                    sleep(2000);
+                    sleep(3000);
                 }catch(Exception e)
                 {
                     e.printStackTrace();
